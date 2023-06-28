b4701f6e81c6eb809c9b071d4ebbfaa0d3b375b3
이권희(huk1833) <gwon_hee@naver.com>
2023-06-28 10:08:54 +0900

    테투

--- src/app/api/slack/route.ts
+++ src/app/api/slack/route.ts
@@ -1,10 +1,23 @@
 import { config } from "@/lib/config";
 import { NextRequest, NextResponse } from "next/server";
+import { parse } from "querystring";
+
 const CHAT_URL = "https://slack.com/api/chat.postMessage";
 export async function POST(req: NextRequest) {
   try {
-    const payload = await req.json();
-    console.log("페이 : ", payload);
+    const reader = req.body!.getReader();
+    let result = await reader.read();
+    let decoder = new TextDecoder("utf-8");
+    let body = "";
+
+    while (!result.done) {
+      body += decoder.decode(result.value);
+      result = await reader.read();
+    }
+
+    const payload = parse(body);
+
+    console.log(payload);
     return NextResponse.json("ok");
   } catch (e) {}
 }
